ea29ba0399e6fce2386b9f48f48ce11a8a86ceaa
Kevin Lorengel <drachenseeletv@gmail.com>
2023-10-26 21:56:53 +0200

    feat: :sparkles: Added Manual Presets

--- super_fast_render/SFR_Operators.py
+++ super_fast_render/SFR_Operators.py
@@ -23,6 +23,307 @@ class SFR_OT_Benchmark_Animation(Operator):
         print("Benchmarking animation")
         return {'FINISHED'}
 
+class SFR_OT_Preset_Preview(Operator):
+    bl_idname = "superfastrender.preset_preview"
+    bl_label = "Preview Preset"
+    bl_description = ""
+
+    def execute(self, context: Context):
+        cycles = bpy.context.scene.cycles
+        render = bpy.context.scene.render
+
+        cycles.use_adaptive_sampling = True
+        cycles.adaptive_threshold = 0.5
+        cycles.adaptive_min_samples = 0
+        cycles.samples = 1024
+        cycles.time_limit = 0
+        cycles.use_preview_adaptive_sampling = False
+        cycles.preview_samples = 32
+        render.preview_pixel_size = '8'
+        
+        cycles.max_bounces = 0
+        cycles.diffuse_bounces = 0
+        cycles.glossy_bounces = 0
+        cycles.transmission_bounces = 0
+        cycles.volume_bounces = 0
+        cycles.transparent_max_bounces = 0
+        cycles.caustics_reflective = False
+        cycles.caustics_refractive = False
+        cycles.sample_clamp_direct = 0
+        cycles.sample_clamp_indirect = 10
+        cycles.use_light_tree = True
+        cycles.auto_scrambling_distance = False
+        cycles.scrambling_distance = 0.6
+        cycles.preview_scrambling_distance = True
+
+        cycles.volume_step_rate = 10
+        cycles.volume_preview_step_rate = 10
+        cycles.volume_max_steps = 64
+
+        cycles.use_auto_tile = True
+        cycles.tile_size = 2048
+
+        cycles.debug_use_spatial_splits = True
+        cycles.debug_use_hair_bvh = True
+        render.use_persistent_data = True
+
+        render.use_simplify = True
+        render.simplify_subdivision = 0
+        render.simplify_child_particles = 0.1
+        cycles.texture_limit = "128"
+        render.simplify_volumes = 0.1
+        render.simplify_subdivision_render = 2
+        render.simplify_child_particles_render = 0.1
+        cycles.texture_limit_render = "128"
+
+        cycles.use_camera_cull = True
+        cycles.camera_cull_margin = 0.1
+        cycles.use_distance_cull = True
+        cycles.distance_cull_margin = 50
+
+        self.report({'INFO'}, "Applied Preview Preset")
+
+        return {'FINISHED'}
+
+class SFR_OT_Preset_Fast(Operator):
+    bl_idname = "superfastrender.preset_fast"
+    bl_label = "Fast Preset"
+    bl_description = ""
+
+    def execute(self, context: Context):
+        cycles = bpy.context.scene.cycles
+        render = bpy.context.scene.render
+
+        cycles.use_adaptive_sampling = True
+        cycles.adaptive_threshold = 0.1
+        cycles.adaptive_min_samples = 0
+        cycles.samples = 1024
+        cycles.time_limit = 0
+        cycles.use_preview_adaptive_sampling = False
+        cycles.preview_samples = 32
+        render.preview_pixel_size = '2'
+        
+        cycles.max_bounces = 3
+        cycles.diffuse_bounces = 1
+        cycles.glossy_bounces = 1
+        cycles.transmission_bounces = 1
+        cycles.volume_bounces = 0
+        cycles.transparent_max_bounces = 1
+        cycles.caustics_reflective = False
+        cycles.caustics_refractive = False
+        cycles.sample_clamp_direct = 0
+        cycles.sample_clamp_indirect = 10
+        cycles.use_light_tree = True
+        cycles.auto_scrambling_distance = False
+        cycles.scrambling_distance = 0.8
+        cycles.preview_scrambling_distance = True
+
+        cycles.volume_step_rate = 5
+        cycles.volume_preview_step_rate = 10
+        cycles.volume_max_steps = 128
+
+        cycles.use_auto_tile = True
+        cycles.tile_size = 2048
+
+        cycles.debug_use_spatial_splits = True
+        cycles.debug_use_hair_bvh = True
+        render.use_persistent_data = True
+
+        render.use_simplify = True
+        render.simplify_subdivision = 1
+        render.simplify_child_particles = 0.1
+        cycles.texture_limit = "512"
+        render.simplify_volumes = 0.3
+        render.simplify_subdivision_render = 3
+        render.simplify_child_particles_render = 0.5
+        cycles.texture_limit_render = "512"
+
+        cycles.use_camera_cull = True
+        cycles.camera_cull_margin = 0.1
+        cycles.use_distance_cull = True
+        cycles.distance_cull_margin = 150
+
+        self.report({'INFO'}, "Applied Fast Preset")
+
+        return {'FINISHED'}
+
+class SFR_OT_Preset_Default(Operator):
+    bl_idname = "superfastrender.preset_default"
+    bl_label = "Default Preset"
+    bl_description = ""
+
+    def execute(self, context: Context):
+        cycles = bpy.context.scene.cycles
+        render = bpy.context.scene.render
+
+        cycles.use_adaptive_sampling = True
+        cycles.adaptive_threshold = 0.01
+        cycles.adaptive_min_samples = 0
+        cycles.samples = 4096
+        cycles.time_limit = 0
+        cycles.use_preview_adaptive_sampling = True
+        cycles.preview_adaptive_threshold = 0.1
+        cycles.preview_adaptive_min_samples = 0
+        cycles.preview_samples = 1024
+        render.preview_pixel_size = 'AUTO'
+
+        cycles.max_bounces = 12
+        cycles.diffuse_bounces = 4
+        cycles.glossy_bounces = 4
+        cycles.transmission_bounces = 12
+        cycles.volume_bounces = 0
+        cycles.transparent_max_bounces = 8
+        cycles.caustics_reflective = True
+        cycles.caustics_refractive = True
+        cycles.blur_glossy = 1
+        cycles.sample_clamp_direct = 0
+        cycles.sample_clamp_indirect = 10
+        cycles.use_light_tree = True
+        cycles.light_sampling_threshold = 0.01
+        cycles.auto_scrambling_distance = False
+        cycles.scrambling_distance = 1
+        cycles.preview_scrambling_distance = False
+
+        cycles.volume_step_rate = 1
+        cycles.volume_preview_step_rate = 1
+        cycles.volume_max_steps = 1024
+
+        cycles.use_auto_tile = True
+        cycles.tile_size = 2048
+
+        cycles.debug_use_spatial_splits = False
+        cycles.debug_use_hair_bvh = True
+        render.use_persistent_data = False
+
+        render.use_simplify = False
+        render.simplify_subdivision = 6
+        render.simplify_child_particles = 1
+        cycles.texture_limit = "OFF"
+        render.simplify_volumes = 1
+        render.simplify_subdivision_render = 6
+        render.simplify_child_particles_render = 1
+        cycles.texture_limit_render = "OFF"
+
+        cycles.use_camera_cull = False
+        cycles.camera_cull_margin = 0.1
+        cycles.use_distance_cull = False
+        cycles.distance_cull_margin = 50
+
+        self.report({'INFO'}, "Applied Blender Default Preset")
+
+        return {'FINISHED'}
+
+class SFR_OT_Preset_High(Operator):
+    bl_idname = "superfastrender.preset_high"
+    bl_label = "High Preset"
+    bl_description = ""
+
+    def execute(self, context: Context):
+        cycles = bpy.context.scene.cycles
+        render = bpy.context.scene.render
+
+        cycles.use_adaptive_sampling = True
+        cycles.adaptive_threshold = 0.01
+        cycles.adaptive_min_samples = 0
+        cycles.samples = 4096
+        cycles.time_limit = 0
+        cycles.use_preview_adaptive_sampling = True
+        cycles.preview_adaptive_threshold = 0.1
+        cycles.preview_adaptive_min_samples = 0
+        cycles.preview_samples = 1024
+        render.preview_pixel_size = 'AUTO'
+
+        cycles.max_bounces = 42
+        cycles.diffuse_bounces = 8
+        cycles.glossy_bounces = 8
+        cycles.transmission_bounces = 24
+        cycles.volume_bounces = 2
+        cycles.transparent_max_bounces = 24
+        cycles.caustics_reflective = True
+        cycles.caustics_refractive = True
+        cycles.blur_glossy = 0.5
+        cycles.sample_clamp_direct = 0
+        cycles.sample_clamp_indirect = 50
+        cycles.use_light_tree = True
+        cycles.auto_scrambling_distance = False
+        cycles.scrambling_distance = 0.9
+        cycles.preview_scrambling_distance = False
+
+        cycles.volume_step_rate = 1
+        cycles.volume_preview_step_rate = 1
+        cycles.volume_max_steps = 1024
+
+        cycles.use_auto_tile = True
+        cycles.tile_size = 2048
+
+        cycles.debug_use_spatial_splits = True
+        cycles.debug_use_hair_bvh = True
+        render.use_persistent_data = True
+
+        render.use_simplify = False
+
+        cycles.use_camera_cull = True
+        cycles.camera_cull_margin = 0.1
+        cycles.use_distance_cull = False
+
+        self.report({'INFO'}, "Applied High Preset")
+
+        return {'FINISHED'}
+
+class SFR_OT_Preset_Ultra(Operator):
+    bl_idname = "superfastrender.preset_ultra"
+    bl_label = "Ultra Preset"
+    bl_description = ""
+
+    def execute(self, context: Context):
+        cycles = bpy.context.scene.cycles
+        render = bpy.context.scene.render
+
+        cycles.use_adaptive_sampling = True
+        cycles.adaptive_threshold = 0.001
+        cycles.adaptive_min_samples = 0
+        cycles.samples = 8192
+        cycles.time_limit = 0
+        cycles.use_preview_adaptive_sampling = False
+        cycles.preview_samples = 0
+        render.preview_pixel_size = '1'
+
+        cycles.max_bounces = 264
+        cycles.diffuse_bounces = 64
+        cycles.glossy_bounces = 64
+        cycles.transmission_bounces = 128
+        cycles.volume_bounces = 8
+        cycles.transparent_max_bounces = 256
+        cycles.caustics_reflective = True
+        cycles.caustics_refractive = True
+        cycles.blur_glossy = 0
+        cycles.sample_clamp_direct = 0
+        cycles.sample_clamp_indirect = 0
+        cycles.use_light_tree = True
+        cycles.auto_scrambling_distance = False
+        cycles.scrambling_distance = 1
+        cycles.preview_scrambling_distance = False
+
+        cycles.volume_step_rate = 0.5
+        cycles.volume_preview_step_rate = 0.5
+        cycles.volume_max_steps = 2048
+
+        cycles.use_auto_tile = True
+        cycles.tile_size = 2048
+
+        cycles.debug_use_spatial_splits = True
+        cycles.debug_use_hair_bvh = True
+        render.use_persistent_data = True
+
+        render.use_simplify = False
+
+        cycles.use_camera_cull = False
+        cycles.use_distance_cull = False
+
+        self.report({'INFO'}, "Applied Ultra Preset")
+
+        return {'FINISHED'}
+
 class SFR_OT_Texture_Optimization(Operator):
     bl_idname = "superfastrender.texture_optimization"
     bl_label = "Texture Optimization"
@@ -62,6 +363,11 @@ class SFR_OT_Mesh_Optimization_Remove(Operator):
 classes = (
     SFR_OT_Benchmark_Frame,
     SFR_OT_Benchmark_Animation,
+    SFR_OT_Preset_Preview,
+    SFR_OT_Preset_Fast,
+    SFR_OT_Preset_Default,
+    SFR_OT_Preset_High,
+    SFR_OT_Preset_Ultra,
     SFR_OT_Texture_Optimization,
     SFR_OT_Mesh_Optimization_Frame,
     SFR_OT_Mesh_Optimization_Animation,
--- super_fast_render/SFR_Panel.py
+++ super_fast_render/SFR_Panel.py
@@ -20,6 +20,8 @@ class SFR_PT_General_Panel(PTB_PT_Panel, Panel):
     def draw(self, context: Context):
         scene = context.scene
         settings = scene.sfr_settings
+        cycles = bpy.context.scene.cycles
+        render = bpy.context.scene.render
 
         colmain = self.layout.column(align=False)
         boxmain = colmain.box()
@@ -69,7 +71,117 @@ class SFR_PT_General_Panel(PTB_PT_Panel, Panel):
                 boxrow.scale_y = 1.5
                 boxrow.operator("superfastrender.benchmark_frame", text="Benchmark Frame", icon="RENDER_STILL")
                 boxrow.operator("superfastrender.benchmark_animation", text="Benchmark Animation", icon="RENDER_ANIMATION")
-        
+
+            if settings.optimization_method == "MANUAL":
+                col = boxcol_rso.column()
+                col.operator("superfastrender.preset_preview", text="Preview Preset", icon="IPO_SINE")
+                col.operator("superfastrender.preset_fast", text="Fast Preset", icon="IPO_QUAD")
+                col.operator("superfastrender.preset_default", text="Default Preset", icon="IPO_CUBIC")
+                col.operator("superfastrender.preset_high", text="High Preset", icon="IPO_QUART")
+                col.operator("superfastrender.preset_ultra", text="Ultra Preset", icon="IPO_QUINT")
+
+                box_settings = boxmain.box()
+                col_settings = box_settings.column(align=True)
+                col_settings.label(text="Render")
+                col_settings.prop(cycles, "use_adaptive_sampling", text="Adaptive Samples", toggle=True)
+                row_settings_adaptive = col_settings.row(align=True)
+                row_settings_adaptive.enabled = cycles.use_adaptive_sampling
+                row_settings_adaptive.prop(cycles, "adaptive_threshold", text="Noise Threshold")
+                row_settings_adaptive.prop(cycles, "adaptive_min_samples", text="Min Samples")
+                row_settings = col_settings.row(align=True)
+                row_settings.prop(cycles, "time_limit", text="Time Limit")
+                row_settings.prop(cycles, "samples", text="Max Samples")
+
+                col_settings = box_settings.column(align=True)
+                col_settings.label(text="Viewport")
+                col_settings.prop(cycles, "use_preview_adaptive_sampling", text="Adaptive Samples", toggle=True)
+                row_settings_adaptive = col_settings.row(align=True)
+                row_settings_adaptive.enabled = cycles.use_preview_adaptive_sampling
+                row_settings_adaptive.prop(cycles, "preview_adaptive_threshold", text="Noise Threshold")
+                row_settings_adaptive.prop(cycles, "preview_adaptive_min_samples", text="Min Samples")
+                col_settings.prop(cycles, "preview_samples", text="Max Samples")
+                col_settings.prop(render, "preview_pixel_size", text="Pixel Size")
+
+                box_settings = boxmain.box()
+                col_settings = box_settings.column()
+                col_settings.prop(cycles, "max_bounces", text="Total Bounces")
+                col_settings = box_settings.column(align=True)
+                col_settings.prop(cycles, "diffuse_bounces", text="Diffuse Bounces")
+                col_settings.prop(cycles, "glossy_bounces", text="Glossy Bounces")
+                col_settings.prop(cycles, "transmission_bounces", text="Transmission Bounces")
+                col_settings.prop(cycles, "volume_bounces", text="Volume Bounces")
+                col_settings = box_settings.column()
+                col_settings.prop(cycles, "transparent_max_bounces", text="Transparent Bounces")
+                row_settings = box_settings.row()
+                row_settings.prop(cycles, "caustics_reflective", text="Reflective Caustics", toggle=True)
+                row_settings.prop(cycles, "caustics_refractive", text="Refractive Caustics", toggle=True)
+                row_settings.prop(cycles, "blur_glossy", text="Blur Caustics", slider=True)
+                row_settings = box_settings.row()
+                row_settings.prop(cycles, "sample_clamp_direct", text="Max Direct Brightness", toggle=True)
+                row_settings.prop(cycles, "sample_clamp_indirect", text="Max Indirect Brightness", toggle=True)
+                col_settings = box_settings.column()
+                col_settings.prop(cycles, "use_light_tree", text="Light Tree", toggle=True)
+                col_settings_lt = col_settings.column()
+                col_settings_lt.enabled = not cycles.use_light_tree
+                col_settings_lt.prop(cycles, "light_sampling_threshold", text="Light Sampling Threshold")
+                row_settings = box_settings.row()
+                row_settings.prop(cycles, "auto_scrambling_distance", text="Auto Scrambling", toggle=True)
+                row_settings.prop(cycles, "scrambling_distance", text="Scrambling Distance", slider=True)    
+                row_settings.prop(cycles, "preview_scrambling_distance", text="Viewport Scrambling", toggle=True)
+
+                box_settings = boxmain.box()
+                col_settings = box_settings.column(align=True)
+                col_settings.prop(cycles, "volume_step_rate", text="Volume Render Step Rate", slider=True)
+                col_settings.prop(cycles, "volume_preview_step_rate", text="Volume Preview Step Rate", slider=True)
+                col_settings.prop(cycles, "volume_max_steps", text="Volume Max Steps")
+
+                box_settings = boxmain.box()
+                row_settings = box_settings.row()
+                row_settings.prop(cycles, "use_auto_tile", text="Use Tiled Rendering", toggle=True)
+                row_settings_tiles = row_settings.row()
+                row_settings_tiles.enabled = cycles.use_auto_tile
+                row_settings_tiles.prop(cycles, "tile_size", text="Tile Size")
+
+                box_settings = boxmain.box()
+                col_settings = box_settings.column()
+                row_settings = col_settings.row()
+                row_settings.prop(cycles, "debug_use_spatial_splits", text="Spacial Splits", toggle=True)
+                row_settings.prop(cycles, "debug_use_hair_bvh", text="Optimize Curves", toggle=True)
+                row_settings.prop(render, "use_persistent_data", text="Keep Render Data", toggle=True)
+                
+                box_settings = boxmain.box()
+                col_settings = box_settings.column(align=True)
+                col_settings.prop(render, "use_simplify", text="Simplify", toggle=True)
+                row_settings_simplify = col_settings.row()
+                row_settings_simplify.enabled = render.use_simplify
+
+                col_settings_simplify = row_settings_simplify.column(align=True)
+                col_settings_simplify.label(text="Viewport")
+                col_settings_simplify.prop(render, "simplify_subdivision", text="Subdivision")
+                col_settings_simplify.prop(render, "simplify_child_particles", text="Child Particles")
+                col_settings_simplify.prop(cycles, "texture_limit", text="Texture Limit")
+                col_settings_simplify.prop(render, "simplify_volumes", text="Volume Resolution", slider=True)
+                col_settings_simplify = row_settings_simplify.column(align=True)
+                col_settings_simplify.label(text="Render")
+                col_settings_simplify.prop(render, "simplify_subdivision_render", text="Subdivision")
+                col_settings_simplify.prop(render, "simplify_child_particles_render", text="Child Particles Render")
+                col_settings_simplify.prop(cycles, "texture_limit_render", text="Texture Limit")
+                
+                box_settings = boxmain.box()
+                col_settings = box_settings.column()
+                row_settings = col_settings.row(align=True)
+                row_settings.prop(cycles, "use_camera_cull", text="Frustrum Culling", toggle=True)
+                row_settings_fc = row_settings.row()
+                row_settings_fc.enabled = cycles.use_camera_cull
+                row_settings_fc.prop(cycles, "camera_cull_margin", text="Frustrum Margin")
+                
+                row_settings = col_settings.row(align=True)
+                row_settings.prop(cycles, "use_distance_cull", text="Distance Culling", toggle=True)
+                row_settings_dc = row_settings.row()
+                row_settings_dc.enabled = cycles.use_distance_cull
+                row_settings_dc.prop(cycles, "distance_cull_margin", text="Distance Margin")
+                
+
         boxmain = colmain.box()
         template_boxtitle(settings, boxmain, "to", "Texture Optimization", "TEXTURE")
         if settings.show_to:
