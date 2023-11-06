17881998fb2eae396bbfbc9b7685fe0250d9f0e2
Kevin Lorengel <drachenseeletv@gmail.com>
2023-11-06 23:47:39 +0100

    Fixed a bug with SPM not registering

--- super_project_manager/SPM_init.py
+++ super_project_manager/SPM_init.py
@@ -2,11 +2,16 @@ from . import (
     SPM_Settings,
     SPM_Operators,
     SPM_Panel,
+    SPM_Functions,
 )
 def register_function():
+    SPM_Functions.setup_addons_data()
+    
     SPM_Settings.register_function()
     SPM_Operators.register_function()
     SPM_Panel.register_function()
+
+    SPM_Functions.register_properties()
 
 
 def unregister_function():
